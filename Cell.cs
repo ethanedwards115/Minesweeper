39c9f181a7d7656ca059860775bd8df202f6d05c
Ethan Edwards <55392603+ethanedwards115@users.noreply.github.com>
2021-02-04 13:55:07 +0000

    initial branch commit; created cell class with base fields

---
+++ Cell.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minesweeper
+{
+    class Cell
+    {
+        private bool _revealed;
+        private int _x;
+        private int _y;
+
+        public Cell(int x, int y)
+        {
+            _x = x;
+            _y = y;
+            _revealed = false;
+        }
+    }
+}
