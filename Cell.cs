d4cff9a71e8c356ae1113e33b505835b4fac9c4b
Ethan Edwards <55392603+ethanedwards115@users.noreply.github.com>
2021-02-09 11:41:59 +0000

    created Cell and Grid class

---
+++ Cell.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minesweeper
+{
+    class Cell
+    {
+        private int x;
+        private int y;
+        private bool revealed;
+        private bool hasMine;
+        private int mineCount;
+
+        public Cell(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+            revealed = false;
+            hasMine = false;
+            mineCount = 0;
+        }
+
+        public int getX()
+        {
+            return x;
+        }
+
+        public int getY()
+        {
+            return y;
+        }
+
+        public bool isRevealed()
+        {
+            return revealed;
+        }
+
+        public bool isMine()
+        {
+            return hasMine;
+        }
+    }
+}
