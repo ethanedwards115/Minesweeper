9ed46b4dc09b27fc44875d0812fa9fb267af0e53
euandocking <55397929+euandocking@users.noreply.github.com>
2021-02-10 14:54:08 +0000

    added an activate-cell function

---
+++ Cell.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minesweeper
+{
+    class Cell
+    {
+        private int x;
+        private int y;
+        private bool revealed;
+        private bool hasMine;
+        private int minesAroundCell;
+
+        public Cell(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+            revealed = false;
+            hasMine = false;
+            minesAroundCell = 0;
+        }
+
+        public int GetX()
+        {
+            return x;
+        }
+
+        public int GetY()
+        {
+            return y;
+        }
+
+        public bool IsRevealed()
+        {
+            return revealed;
+        }
+
+        public bool IsMine()
+        {
+            return hasMine;
+        }
+
+        public int GetMinesAroundCell()
+        {
+            return minesAroundCell;
+        }
+
+        public void SetMinesAroundCell(int adjacentMines)
+        {
+            minesAroundCell = adjacentMines;
+        }
+    }
+}
---
+++ Grid.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minesweeper
+{
+    class Grid
+    {
+        private Cell[,] cells;
+        private int width;
+        private int height;
+        private int mineCount;
+
+        public Grid(int _w, int _h, int _mineCount)
+        {
+            width = _w;
+            height = _h;
+            mineCount = _mineCount;
+
+            cells = new Cell[width, height];
+
+            InitCells(cells);
+        }
+
+        private void InitCells(Cell[,] cells)
+        {
+            for (int col = 0; col < width; col++)
+            {
+                for (int row = 0; row < height; row++)
+                {
+                    cells[col, row] = new Cell(col, row);
+                }
+            }
+        }
+
+        public void ActivateCell(int col, int row)
+        {
+            if (cells[col, row].IsMine())
+            {
+                for (int i = 0; i < width; i++)
+                {
+                    for (int j = 0; j < height; j++)
+                    {
+                        if (cells[i, j].IsMine())
+                        {
+                            //display mine image on button
+                        }
+                    }
+                }
+                Console.Write("Game Over");
+            }
+            else
+            {
+                CountAdjacentMines(col, row);
+            }
+        }
+
+        public void CountAdjacentMines(int col, int row)
+        {
+            int adjacentMines = 0;
+            for (int x = -1; x <= 1; x++)
+            {
+                for (int y = -1; y <= 1; y++)
+                {
+                    if (!(x == 0 && y == 0))
+                    {
+                        try
+                        {
+                            Cell currentCell = cells[col + x, row + y];
+                            if (currentCell.IsMine())
+                            {
+                                adjacentMines++;
+                            }
+                        }
+                        catch (IndexOutOfRangeException e)
+                        {
+
+                        }
+                    }
+                }
+            }
+            cells[col, row].SetMinesAroundCell(adjacentMines);
+        }
+    }
+}
